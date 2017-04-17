7225d802250147e075794b4100b4efa898096d11
Irvin <irvink.tang@gmail.com>
2017-04-17 11:05:22 -0700

    format

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,149 +1,179 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var rsb = require('react-bootstrap');
+var React = require("react");
+var ReactDOM = require("react-dom");
+var rsb = require("react-bootstrap");
 
-import {Modal, Button} from 'react-bootstrap'
+import { Modal, Button } from "react-bootstrap";
 
 class Jobs extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: '',
-      reqId: '',
-      reqStatus:'',
-      reqInfo: '',
-      jobId: '',
+      url: "",
+      reqId: "",
+      reqStatus: "",
+      reqInfo: "",
+      jobId: "",
       jobData: {},
       showJobId: false,
       showStatus: false
-    }
+    };
 
-    this.submitJob = this.submitJob.bind(this)
-    this.changeUrl = this.changeUrl.bind(this)
-    this.changeId = this.changeId.bind(this)
-    this.requestJob = this.requestJob.bind(this)
-    this.closeJob = this.closeJob.bind(this)
-    this.closeStatus = this.closeStatus.bind(this)
+    this.submitJob = this.submitJob.bind(this);
+    this.changeUrl = this.changeUrl.bind(this);
+    this.changeId = this.changeId.bind(this);
+    this.requestJob = this.requestJob.bind(this);
+    this.closeJob = this.closeJob.bind(this);
+    this.closeStatus = this.closeStatus.bind(this);
   }
 
   componentDidMount() {
-    fetch('/data')
-      .then((res) => res.json())
-      .then((json)=> {
-        this.setState({jobData: json["data"]})
-      })
+    fetch("/data").then(res => res.json()).then(json => {
+      this.setState({ jobData: json["data"] });
+    });
   }
 
   submitJob(event) {
     event.preventDefault();
-    var data = JSON.stringify({url: this.state.url});
-    fetch('/jobs', {
-      method: 'POST',
+    var data = JSON.stringify({ url: this.state.url });
+    fetch("/jobs", {
+      method: "POST",
       headers: {
-        'Accept':'application/json',
-        'Content-Type':'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json"
       },
       body: data
     })
-    .then((res) => res.json())
-    .then((json) => {
-      this.setState({jobId: json["id"].id, jobData:json["data"], showJobId: true})
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          jobId: json["id"].id,
+          jobData: json["data"],
+          showJobId: true
+        });
       })
       .catch(function(err) {
-        console.log('ERROR: ', err);
-      })
-    }
-
-    requestJob(event) {
-      event.preventDefault();
-      fetch(`/jobs/${this.state.reqId}`)
-        .then((res) => res.json())
-        .then((json) => {
-          var data = JSON.parse(json)
-          this.setState({reqStatus:data.status, reqInfo: data.html || 'Still processing...', showStatus: true})
-        })
-    }
-
-    changeUrl(event) {
-      this.setState({url: event.target.value})
-    }
-
-    changeId(event) {
-      this.setState({reqId: event.target.value})
-    }
-
-    closeJob() {
-      this.setState({showJobId: false})
-    }
-
-    closeStatus() {
-      this.setState({showStatus: false})
-    }
-
-    render() {
-      return (
-        <div>
-          <div style={{display:"flex",flexDirection:"column"}} className="col-sm-4">
-            <div>
-              <form className="form-group" onSubmit={this.submitJob}>
-                <h2 className="form-signin-heading">Submit Job</h2>
-                <h3>Must be an absolute url, eg http:&#47;&#47;example.com&#47;</h3>
-                <input value={this.state.url} className="form-control" onChange={this.changeUrl} type="text" required placeholder="http://example.com/"></input>
-                <button className="btn btn-default" type="submit">Submit</button>
-              </form>
-            </div>
-            <div>
-              <form className="form-group" onSubmit={this.requestJob}>
-                <h2>Insert Job ID</h2>
-                <input value={this.state.reqId} className="form-control" onChange={this.changeId} type="text" required placeholder="Enter Job ID"></input>
-                <button className="btn btn-default" type="submit">Request Job</button>
-              </form>
-            </div>
-          </div>
-          <div className="col-sm-8">
-            <table className="table table-bordered">
-              <thead>
-                <tr>
-                  <th>URL</th>
-                  <th>Job ID</th>
-                </tr>
-              </thead>
-              <tbody>
-              {this.state.jobData ? Object.keys(this.state.jobData).map((jobId) => {
-                var data = this.state.jobData
-                return <tr><td>{JSON.parse(data[jobId]).url}</td><td>{jobId}</td></tr>
-              }) : null}
-              </tbody>
-            </table>
-          </div>
-
-          <Modal show={this.state.showJobId} onHide={this.closeJob}>
-            <Modal.Header closeButton>
-              <Modal.Title>Job ID: {this.state.jobId}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <h4>Use this ID to access status of submitted job</h4>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={this.closeJob}>Close</Button>
-            </Modal.Footer>
-          </Modal>
-
-          <Modal show={this.state.showStatus} onHide={this.closeStatus}>
-            <Modal.Header closeButton>
-              <Modal.Title>Requested Job:{this.state.reqId}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              <h2>Job Status: {this.state.reqStatus}</h2>
-              <p>{this.state.reqInfo}</p>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button onClick={this.closeStatus}>Close</Button>
-            </Modal.Footer>
-          </Modal>
-        </div>
-      )
-    }
+        console.log("ERROR: ", err);
+      });
   }
 
-  ReactDOM.render(<Jobs />, document.getElementById('root'))
+  requestJob(event) {
+    event.preventDefault();
+    fetch(`/jobs/${this.state.reqId}`).then(res => res.json()).then(json => {
+      var data = JSON.parse(json);
+      this.setState({
+        reqStatus: data.status,
+        reqInfo: data.html || "Still processing...",
+        showStatus: true
+      });
+    });
+  }
+
+  changeUrl(event) {
+    this.setState({ url: event.target.value });
+  }
+
+  changeId(event) {
+    this.setState({ reqId: event.target.value });
+  }
+
+  closeJob() {
+    this.setState({ showJobId: false });
+  }
+
+  closeStatus() {
+    this.setState({ showStatus: false });
+  }
+
+  render() {
+    return (
+      <div>
+        <div
+          style={{ display: "flex", flexDirection: "column" }}
+          className="col-sm-4"
+        >
+          <div>
+            <form className="form-group" onSubmit={this.submitJob}>
+              <h2 className="form-signin-heading">Submit Job</h2>
+              <h3>Must be an absolute url, eg http://example.com/</h3>
+              <input
+                value={this.state.url}
+                className="form-control"
+                onChange={this.changeUrl}
+                type="text"
+                required
+                placeholder="http://example.com/"
+              />
+              <button className="btn btn-default" type="submit">Submit</button>
+            </form>
+          </div>
+          <div>
+            <form className="form-group" onSubmit={this.requestJob}>
+              <h2>Insert Job ID</h2>
+              <input
+                value={this.state.reqId}
+                className="form-control"
+                onChange={this.changeId}
+                type="text"
+                required
+                placeholder="Enter Job ID"
+              />
+              <button className="btn btn-default" type="submit">
+                Request Job
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="col-sm-8">
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th>URL</th>
+                <th>Job ID</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.jobData
+                ? Object.keys(this.state.jobData).map(jobId => {
+                    var data = this.state.jobData;
+                    return (
+                      <tr>
+                        <td>{JSON.parse(data[jobId]).url}</td>
+                        <td>{jobId}</td>
+                      </tr>
+                    );
+                  })
+                : null}
+            </tbody>
+          </table>
+        </div>
+
+        <Modal show={this.state.showJobId} onHide={this.closeJob}>
+          <Modal.Header closeButton>
+            <Modal.Title>Job ID: {this.state.jobId}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Use this ID to access status of submitted job</h4>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeJob}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+
+        <Modal show={this.state.showStatus} onHide={this.closeStatus}>
+          <Modal.Header closeButton>
+            <Modal.Title>Requested Job:{this.state.reqId}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h2>Job Status: {this.state.reqStatus}</h2>
+            <p>{this.state.reqInfo}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeStatus}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Jobs />, document.getElementById("root"));
--- server.js
+++ server.js
@@ -1,76 +1,88 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var fetch = require('node-fetch')
-var app = express()
-var path = require('path')
-var redis = require('redis')
-var uuid = require('uuid/v1')
+var express = require("express");
+var bodyParser = require("body-parser");
+var fetch = require("node-fetch");
+var app = express();
+var path = require("path");
+var redis = require("redis");
+var uuid = require("uuid/v1");
 
-app.use(express.static('build'))
+app.use(express.static("build"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-if(! process.env.REDIS_URL) {
-  console.error('Missing environment variable: REDIS_URL');
+if (!process.env.REDIS_URL) {
+  console.error("Missing environment variable: REDIS_URL");
 }
 
 var client = redis.createClient(process.env.REDIS_URL);
-client.on('error', function(err){
-  console.log("ERROR:", err)
-})
-client.on('connect', function(reply){
-  console.log('connect')
-})
+client.on("error", function(err) {
+  console.log("ERROR:", err);
+});
+client.on("connect", function(reply) {
+  console.log("connect");
+});
 
-app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname, 'index.html'))
-})
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get('/data', function(req,res){
-  client.hgetall('jobs', function(err,jobs){
-    res.json({data: jobs})
-  })
-})
+app.get("/data", function(req, res) {
+  client.hgetall("jobs", function(err, jobs) {
+    res.json({ data: jobs });
+  });
+});
 
-app.get('/jobs/:id',function(req,res){
-  client.hget('jobs', req.params.id, function(err,job){
-    res.json(job)
-  })
-})
+app.get("/jobs/:id", function(req, res) {
+  client.hget("jobs", req.params.id, function(err, job) {
+    res.json(job);
+  });
+});
 
-app.post('/jobs', function(req,res){
+app.post("/jobs", function(req, res) {
   var jobId = uuid();
-  client.rpush('jobQueue', JSON.stringify({url: req.body.url, id: jobId}), function(err,reply) {
-    if(err) throw(err)
-    client.hset('jobs', jobId, JSON.stringify({url: req.body.url, status: 'In Progress'}), function(err, reply1) {
-      if(err) throw(err)
-      client.hgetall('jobs', function(err,jobs){
-        res.json({id: {id: jobId},data:jobs})
-      })
-    })
-  })
-})
+  client.rpush(
+    "jobQueue",
+    JSON.stringify({ url: req.body.url, id: jobId }),
+    function(err, reply) {
+      if (err) throw err;
+      client.hset(
+        "jobs",
+        jobId,
+        JSON.stringify({ url: req.body.url, status: "In Progress" }),
+        function(err, reply1) {
+          if (err) throw err;
+          client.hgetall("jobs", function(err, jobs) {
+            res.json({ id: { id: jobId }, data: jobs });
+          });
+        }
+      );
+    }
+  );
+});
 
 function processJob(err, job) {
-  client.blpop('jobQueue',1, processJob)
-  if(err) throw(err)
-  if(job) {
-    var url = JSON.parse(job[1]).url
-    fetch(url)
-    .then(res => res.text())
-    .then(body => {
-      client.hset('jobs',JSON.parse(job[1]).id, JSON.stringify({url: url, status: 'Completed', html: body}), function(err,status){
-        if(err) throw(err)
-        console.log('success')
-      })
-    })
+  client.blpop("jobQueue", 1, processJob);
+  if (err) throw err;
+  if (job) {
+    var url = JSON.parse(job[1]).url;
+    fetch(url).then(res => res.text()).then(body => {
+      client.hset(
+        "jobs",
+        JSON.parse(job[1]).id,
+        JSON.stringify({ url: url, status: "Completed", html: body }),
+        function(err, status) {
+          if (err) throw err;
+          console.log("success");
+        }
+      );
+    });
   } else {
-    console.log(job)
+    console.log(job);
   }
 }
 
-client.blpop('jobQueue',1, processJob)
+client.blpop("jobQueue", 1, processJob);
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log('running')
-})
+app.listen(process.env.PORT || 3000, function() {
+  console.log("running");
+});
